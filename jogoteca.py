87f8b42350f0b7eb0ea26c2d2b610f7d2fbf412c
Gustavo Cardoso <cargugati@gmail.com>
2022-11-06 17:37:35 -0300

    fim

--- jogoteca.py
+++ jogoteca.py
@@ -1,38 +1,62 @@
 
-from flask import Flask, render_template, request, redirect
-
+from flask import Flask, render_template, request, redirect, session, flash
 class Jogo:
     def __init__(self, nome, categoria, console):
         self.nome = nome
         self.categoria = categoria
         self.console = console
 
-jogo1= Jogo ('Tetris', 'Puzzle', 'Atari')
-jogo2= Jogo ('Goo of War', 'Rack n Slash', 'PS2')
-jogo3= Jogo ('Mortal Kombat', 'Luta', 'PS2')
-jogo4= Jogo ('EuroTruck', 'Simulador', 'PS3')
+
+jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
+jogo2 = Jogo('Goo of War', 'Rack n Slash', 'PS2')
+jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
+jogo4 = Jogo('EuroTruck', 'Simulador', 'PS3')
 
 lista = [jogo1, jogo2, jogo3, jogo4]
 
 
 app = Flask(__name__)
+app.secret_key = 'gustavo'
+
 
 @app.route('/')
 def index():
     return render_template('lista.html', titulo='jogos', jogos=lista)
 
+
 @app.route('/novo')
 def novo():
     return render_template('novo.html', titulo='novo jogo')
 
-@app.route('/criar', methods=['POST',])
+
+@app.route('/criar', methods=['POST', ])
 def criar():
     nome = request.form['nome']
     categoria = request.form['categoria']
     console = request.form['console']
 
-    jogo = Jogo(nome,categoria,console)
+    jogo = Jogo(nome, categoria, console)
     lista.append(jogo)
+    return redirect('/')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/autenticar', methods=['POST', ])
+def autenticar():
+    if 'alohomora' == request.form['senha']:
+        session['usuario_logado'] = request.form['usuario']
+        flash(session['usuario_logado'] + ' logado com sucesso!')
+        return redirect('/')
+    else:
+        flash('Usuário não logado.')
+        return redirect('/login')
+
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] = None
+    flash('Logout efetuado com sucesso!')
     return redirect('/')
 
 app.run(debug=True, host='0.0.0.0', port=8080)
