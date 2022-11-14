f3f941928c6544f75dc972a23f7f0a59c5cce48b
Gustavo Cardoso <cargugati@gmail.com>
2022-11-14 19:10:09 -0300

    2-Completando o CRUD

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 # Meu primeiro projeto em Python 3 -> JogoTeca
+
 ## Usando
+
 - Flask 3.0
 - BootStrap 5
+
+- python -m venv venv
+- ./venv/Scripts/Activate
+- pip freeze
+- pip install mysqlclient
+- pip install mysql-connector-python==8.0.28
+- pip install flask-sqlalchemy==2.5.1
--- jogoteca.py
+++ jogoteca.py
@@ -1,91 +1,12 @@
-
-from flask import Flask, render_template, request, redirect, session, flash, url_for
-
-
-class Jogo:
-    def __init__(self, nome, categoria, console):
-        self.nome = nome
-        self.categoria = categoria
-        self.console = console
-
-
-jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
-jogo2 = Jogo('Goo of War', 'Rack n Slash', 'PS2')
-jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
-jogo4 = Jogo('EuroTruck', 'Simulador', 'PS3')
-
-lista = [jogo1, jogo2, jogo3, jogo4]
-
-
-class Usuario:
-    def __init__(self, nome, nickname, senha):
-        self.nome = nome
-        self.nickname = nickname
-        self.senha = senha
-
-
-ususario1 = Usuario("Gustavo Cardoso", "GC", "admin123")
-ususario2 = Usuario("Alexandre G Moraes", "34218417806", "mudar@123")
-ususario3 = Usuario("Margareth Tebas", "28973326899", "mudar@123")
-
-usuarios = {
-    ususario1.nickname: ususario1,
-    ususario2.nickname: ususario2,
-    ususario3.nickname: ususario3, }
-
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.secret_key = 'gustavo'
+app.config.from_pyfile('config.py')
 
+db = SQLAlchemy(app)
 
-@app.route('/')
-def index():
-    return render_template('lista.html', titulo='jogos', jogos=lista)
+from views import *
 
-
-@app.route('/novo')
-def novo():
-    if 'usuario_logado' not in session or session['usuario_logado'] == None:
-        return redirect(url_for('login', proxima=url_for('novo')))
-    return render_template('novo.html', titulo='novo jogo')
-
-
-@app.route('/criar', methods=['POST', ])
-def criar():
-    nome = request.form['nome']
-    categoria = request.form['categoria']
-    console = request.form['console']
-
-    jogo = Jogo(nome, categoria, console)
-    lista.append(jogo)
-    return redirect(url_for('index'))
-
-
-@app.route('/login')
-def login():
-    proxima = request.args.get('proxima')
-    return render_template('login.html', proxima=proxima)
-
-
-@app.route('/autenticar', methods=['POST', ])
-def autenticar():
-    if request.form['usuario'] in usuarios:
-        usuario = usuarios[ request.form['usuario'] ]
-        if request.form['senha'] == usuario.senha:
-            session['usuario_logado'] = usuario.nickname
-            flash( usuario.nome + ' logado com sucesso!')
-            proxima_pagina = request.form['proxima']
-            return redirect(proxima_pagina)
-    else:
-        flash('Usuário não logado.')
-        return redirect(url_for('login'))
-
-
-@app.route('/logout')
-def logout():
-    session['usuario_logado'] = None
-    flash('Logout efetuado com sucesso!')
-    return redirect(url_for('index'))
-
-
-app.run(debug=True, host='0.0.0.0', port=8080)
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8080)  
